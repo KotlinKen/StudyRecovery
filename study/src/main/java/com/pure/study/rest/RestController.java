6bf3d40b10940234d015fb620cfff3b3b8e0d16a
aquar <aquarene@nate.com>
2018-06-21 17:19:40 +0900

    tester

--- study/src/main/java/com/pure/study/rest/RestController.java
+++ study/src/main/java/com/pure/study/rest/RestController.java
@@ -24,7 +24,7 @@ public class RestController {
 	@ResponseBody
 	public List<Map<String,String>> selectAdverstingRest(@RequestParam(value="filter", required=false) String filter) {
 		
-		//1234
+		//12345678
 		List<Map<String,String>> list = studyService.selectStudyList(1, 5);
 		
 		//test
