4a9ace1c90d6aa6d37b956cec7c2496c1fed8b8f
jjsk109 <jjsk109@gmail.com>
2018-07-05 16:39:12 +0900

    20180705회원가입접근

--- study/src/main/java/com/pure/study/common/aop/MemberEnrollAOP.java
+++ study/src/main/java/com/pure/study/common/aop/MemberEnrollAOP.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -26,8 +27,39 @@ public class MemberEnrollAOP {
       logger.info("Controller메소드가 시작했습니다.");
       
       Object[] obj = joinPoint.getArgs();
-      logger.info("Controller메소드가 시작했습니다. : ");
+      logger.info("Controller메소드가 시작했습니다. : "+joinPoint);
+      logger.info("Controller메소드가 시작했습니다.getArgs : "+joinPoint.getArgs());
+      logger.info("Controller메소드가 시작했습니다.obj : "+obj);
+      try {
+		
+    	  logger.info("Controller메소드가 시작했습니다.obj : "+obj[0]);
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+      logger.info("Controller메소드가 시작했습니다.getKind : "+joinPoint.getKind());
+      logger.info("Controller메소드가 시작했습니다.hashCode : "+joinPoint.hashCode());
+      logger.info("Controller메소드가 시작했습니다.toLongString : "+joinPoint.toLongString());
+      logger.info("Controller메소드가 시작했습니다.toString : "+joinPoint.toString());
+      logger.info("Controller메소드가 시작했습니다.getStaticPart : "+joinPoint.getStaticPart());
+      logger.info("Controller메소드가 시작했습니다.getThis : "+joinPoint.getThis());
+      logger.info("Controller메소드가 시작했습니다.getTarget : "+joinPoint.getTarget());
+      logger.info("Controller메소드가 시작했습니다.getSourceLocation : "+joinPoint.getSourceLocation());
+      logger.info("Controller메소드가 시작했습니다.getClass : "+joinPoint.getClass());
+      logger.info("Controller메소드가 시작했습니다.getSignature : "+joinPoint.getSignature());
       
+      Object params[] = joinPoint.getArgs();
+      for(Object param : params) {
+         
+              logger.info("찍힐까? : "+param);
+          
+      }
+      
+     // EnrollInterceptora lciaop = new EnrollInterceptora();
+//      logger.info("Controller메소드가 시작했습니다.lciaop : "+lciaop);
+//      
+//      if(lciaop) {
+//    	  
+//      }
 
       
    }
